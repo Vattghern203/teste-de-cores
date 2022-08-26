9d08df6817b0074983fd60c2f8ab825a762ff1cb
vattghern <otavioaugustobrb19@gmail.com>
2022-08-26 01:23:32 -0300

    jaco update

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from colorsys import rgb_to_hsv
 from PIL import Image, ImageOps
 
-img = Image.open(r"../imgs/photo_1.webp")
+from convert import image_gray_scale, image_rgb_2_hsv
+from defaultFunctions import save_image
+
+'''img = Image.open(r"../imgs/photo_1.webp")
 img2 = Image.open(r"../imgs/img_bmp_16.bmp")
 imgTree = Image.open(r"../imgs/tree7.jpg")
 imgPSD = Image.open(r"../imgs/ImagemOriginal.psd")
@@ -21,7 +24,12 @@ histogramB = b.histogram()
 
 print('R = {} \n\n G = {} \n\n B = {}'.format(histogramR, histogramG, histogramB))
 
-print(img.histogram())
+print(img.histogram())'''
+
+img_jaco = Image.open(r"imgs/jaco.jpg")
+img_jaco_hsv = image_rgb_2_hsv(img_jaco)
+save_image(img_jaco_hsv, "jaco_bolado")
+img_jaco_hsv.show()
 
 
 
